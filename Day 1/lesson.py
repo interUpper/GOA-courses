58db82ab64de31257283d2181f581cc24a88c0d4
interUpper <nika.utiashvili808@gmail.com>
2024-01-30 18:33:25 +0400

    first commit

---
+++ Day 1/lesson.py
@@ -0,0 +1,7 @@
+name = "nika"
+surname = "utiashvili"
+#gadaketdeba str commandit
+age = 16
+is_school_pupil = True
+
+print("me mqvia " + name + "." + " chemi gvaria " + surname + "." + " var " + str(age ) + " wlis " + "davdivar skolashi " + str(is_school_pupil))
