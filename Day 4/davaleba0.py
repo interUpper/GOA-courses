06e150e690cbd5d1dbba2c6ca10e36c28e4a283d
interUpper <nika.utiashvili808@gmail.com>
2024-02-29 14:22:59 +0400

    day4homework

---
+++ Day 4/davaleba0.py
@@ -0,0 +1,6 @@
+ricxvi1 = int(input("enter first number: "))
+ricxvi2 = int(input("enter second number: "))
+
+jami = ricxvi1 + ricxvi2
+
+print("sum of", ricxvi1, "+", ricxvi2, "=", jami)
---
+++ Day 4/davaleba1.py
@@ -0,0 +1,5 @@
+username = input("write ur name: ")
+age = int(input("write ur age"))
+timeskip = int(input("how many years do you want to travel in time: "))
+
+print(username, "you will be ", age+timeskip , "years old :)")
