d994a45a4dd504e9b9fef2e4c8e72c9a957d21c1
interUpper <nika.utiashvili808@gmail.com>
2024-03-10 17:01:24 +0400

    day 5

---
+++ Day 5/homework.py
@@ -0,0 +1,9 @@
+num1 = float(input("write first number here: "))
+num2 = float(input("write second number here: "))
+
+print(type(num1))
+print(type(num2))
+
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
