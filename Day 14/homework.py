c63e9e1a6901291ba2c34681833754251d277708
interUpper <nika.utiashvili808@gmail.com>
2024-05-14 18:57:53 +0400

    Day 15

---
+++ Day 14/homework.py
@@ -0,0 +1,7 @@
+clients = int(input("please enter amount of client in market : "))
+while clients > 0:
+    for i in range:
+        print(clients - 1)
+
+
+        #ar matestinebs da zustad ar vici.
