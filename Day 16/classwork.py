9f8a910dbd5459806305c36b791c33ba4b0c183f
interUpper <nika.utiashvili808@gmail.com>
2024-06-08 12:41:36 +0400

    homework

---
+++ Day 16/classwork.py
@@ -0,0 +1,40 @@
+#komediuri filmebi
+# movies = ["white chicks", "jumanji", "jumanji 2", "big mommy", "marto saxlshi"]
+
+# print(movies[1])
+
+
+# cars = []
+
+# for i in range(5):
+#     car = input("please input carname: ")
+#     cars.append(car)
+# print(cars)
+
+# cars[0] = input("please reset first car name: ")
+
+# print(cars)
+
+
+#შემოვიტანეთ carsსია და ფორ ის მეშვეობით კოდი შემდეგი კოდი გავუშვით 5 ჯერ რაშიც შემოვიტანდით მანქანის სახელებს და დავამატებდით რიგრიგობით
+#შემდეგ დავპრინტეთ სიის შედეგი
+#შემოვიტანეთ ფუნქცია რომელიც გადაწერდა პირველ მანქანას და გამოვიტანეთ შედეგი
+
+
+# cars = ["mustang", "ford", "bmw", "volkswaggen", "bugati"]
+# total = 0
+
+# for car in cars:
+#     total = total + 1
+# print(total)
+
+
+# names = ["ramazi", "anano", "mate", "teo"]
+
+# filtered_names = []
+
+# for name in names:
+#     if len(name) >= 4:
+#         filtered_names.append(names)
+# print(filtered_names)
+
---
+++ Day 18/homework.py
@@ -0,0 +1,7 @@
+list = ["$", "%", "^", "V"]
+#pirveli magaliti
+print(list[1], list[2])
+#meore magaliti
+print(list[1:3])
+#mesame magaliti
+print(list[-2], list[-3])
